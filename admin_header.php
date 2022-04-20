b38a6cb718840d76d029c5e9191f1979dcd31cb4
aaaasif <alexasif4gmail.com>
2022-04-21 00:09:28 +0600

    dashboard header added

--- admin_header.php
+++ admin_header.php
@@ -1,14 +1,7 @@
 <?php
 
 if(isset($message)){
-   foreach($message as $message){
-      echo '
-      <div class="message">
-         <span>'.$message.'</span>
-         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
-      </div>
-      ';
-   }
+   
 }
 
 ?>
